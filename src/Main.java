2ef00c79ed62bd1739c6895cde17a5a7ae857b78
user <athimbityana@gmail com>
2022-06-08 15:05:58 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        String course[] = new String[4]; //declaring a one dimensional array
+
+        //Assigning elements to the array
+
+        course[0] = "Java training for testers";
+        course[1] = "Certified lead pen Test";
+        course[2] = "ISTQB CTFL";
+        course[3] = "Dev0ps foundation";
+
+        try {
+            for (int i = 0; i < 5; i++) {
+
+            }
+        } catch (Exception e) {
+            System.out.println("Exception is" + e);
+        }
+    }
+
+
+        }
